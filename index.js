d3774c2cc7ef4409d197a7083e8a33ca437beb2a
Akshat <iamakku0.0.0.1@gmail.com>
2023-02-24 11:05:16 +0530

    commiting on 24 feb 23

---
+++ index.js
@@ -0,0 +1,26 @@
+// fetching the element numb not value of numb element by query selector
+const value = document.querySelector('#numb');
+
+// making function with arrow function in it no need to write function and function name
+// increment is variable having function
+const increment = ()=>{
+
+    // getting value of fetched element by .innerText method
+     let updatedValue = parseInt(value.innerText); //parsing string value into integer
+    updatedValue++;
+    //setting the fetched value of element by updatedvalue
+    value.innerText=updatedValue;
+  
+    
+};
+
+// decrement is variable having function
+const decrement = ()=>{
+
+     // getting value of fetched element by .innerText method
+    let updatedValue = parseInt(value.innerText); //parsing string value into integer
+    updatedValue--;
+    //setting the fetched value of element by updatedvalue
+    value.innerText=updatedValue;
+ 
+};
